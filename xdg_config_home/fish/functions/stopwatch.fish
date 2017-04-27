205dbab59956e032ac5336966e3c44359b249c10
Dan Callahan <dan.callahan@gmail.com>
2017-04-26 20:33:28 -0500

    Fixup stopwatch command

--- xdg_config_home/fish/functions/stopwatch.fish
+++ xdg_config_home/fish/functions/stopwatch.fish
@@ -2,7 +2,7 @@ function stopwatch --description 'Start counting time'
 	set -l t0 (date -u +%s)
 	while true
 		set -l t1 (date -u +%s)
-		echo -n (date -u -r (math $t1 - $t0) +%H:%M:%S\r)
+		echo -n (date -u --date=@(math $t1 - $t0) +%H:%M:%S\r)
 		sleep 0.1
 	end
 end
