16c43c3a5d16083350dd7e57c0a8846f5084d44f
Dan Callahan <dan.callahan@gmail.com>
2019-09-18 23:28:51 +0100

    Temporarily use modified copy of nixpkgs-mozilla

--- NixOS/.config/nixpkgs/overlays.nix
+++ NixOS/.config/nixpkgs/overlays.nix
@@ -1,5 +1,9 @@
 [
-  (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
+  #(import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
+  # Use a local copy of nixpkgs-mozilla until the following bug is fixed:
+  #     https://github.com/mozilla/nixpkgs-mozilla/issues/199
+  (import ./nixpkgs-mozilla/default.nix)
+
   (import ./my-packages.nix)
   (import ./other-packages/default.nix)
 ]
