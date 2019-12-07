f1c099ed66d471c39c8ec5c4e9cde8f772fce141
Dan Callahan <dan.callahan@gmail.com>
2019-12-07 09:51:49 +0000

    Remove superfluous config for MAC randomization
    I misread the module; scanRandMacAddress *is* on by default in NixOS.

--- NixOS/etc/nixos/modules/system.nix
+++ NixOS/etc/nixos/modules/system.nix
@@ -20,11 +20,6 @@
   i18n.consoleUseXkbConfig = true;
   services.xserver.xkbOptions = "ctrl:nocaps";
 
-  # MAC Address Randomization (During Scans)
-  #  On by default in upstream NetworkManager 1.2, but disabled in NixOS
-  #  See: https://blogs.gnome.org/thaller/2016/08/26/mac-address-spoofing-in-networkmanager-1-4-0/
-  networking.networkmanager.wifi.scanRandMacAddress = true;
-
   # Firewall
   networking.firewall.enable = true;
 
